ae5891a5ba1697b45009f4850c0b79cacba009e8
kMlad <kmlad98@gmail.com>
2023-03-11 13:47:57 +0100

    Finish modules and crates module. Touch on rand crate

--- modules_crates/src/main.rs
+++ modules_crates/src/main.rs
@@ -1,16 +1,33 @@
 mod player;
+mod archive;
 
 use player::play_movie;
 use player::play_audio;
 use clean::perform_clean;
 use clean::cache::clean_cache;
+use crate::archive::arch::arch_file as arc;
+use rand::Rng;
 
 fn main() {
     play_movie("Creed 3.mp4");
     play_audio("Smack that.mp3");
 
+    println!("--------------------------");
+
     perform_clean();
     clean_cache();
+
+    println!("--------------------------");
+
+
+    arc("hehence.rs");
+
+    println!("--------------------------");
+
+
+    let mut rng = rand::thread_rng();
+    let generated_number: u8 = rng.gen_range(1..100);
+    println!("Your number is {:?}", generated_number);
 
 }
 
